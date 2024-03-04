1ce7fa5e790824379c48db42a6516ecbe92f6794
Aashray Munjal <aashraymunjal@gmail.com>
2024-03-03 19:51:00 -0500

    added delete and update API

--- src/main/java/com/munjal/QuizApplication/Controller/QuestionController.java
+++ src/main/java/com/munjal/QuizApplication/Controller/QuestionController.java
@@ -3,9 +3,11 @@ package com.munjal.QuizApplication.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,4 +37,14 @@ public class QuestionController {
 		return questionService.addQuestion(question);
 	}
 
+	@DeleteMapping("/delete/{id}")
+	public String delete(@PathVariable("id") int id) {
+		return questionService.delete(id);
+	}
+
+	@PutMapping("/update/{id}")
+	public Question updateQuestion(@PathVariable("id") int id, @RequestBody Question question) {
+		return questionService.updateQuestion(id, question);
+
+	}
 }
--- src/main/java/com/munjal/QuizApplication/Service/QuestionService.java
+++ src/main/java/com/munjal/QuizApplication/Service/QuestionService.java
@@ -1,10 +1,9 @@
 package com.munjal.QuizApplication.Service;
 
 import java.util.List;
-
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.munjal.QuizApplication.Dao.QuestionDao;
 import com.munjal.QuizApplication.Model.Question;
 
@@ -27,6 +26,33 @@ public class QuestionService {
 	public String addQuestion(Question question) {
 		questionDao.save(question);
 		return "Added: " + question.toString();
+	}
+
+	public String delete(int id) {
+		questionDao.deleteById(id);
+		return "Deleted: " + id;
+	}
+
+	public Question updateQuestion(int id, Question question) {
+		Optional<Question> q = questionDao.findById(id);
+		Question old = new Question();
+		if (questionDao.findById(id).isPresent()) {
+			old = q.get();
+			old.setQuestionTitle(question.getQuestionTitle());
+			old.setOption1(question.getOption1());
+			old.setOption2(question.getOption2());
+			old.setOption3(question.getOption3());
+			old.setOption4(question.getOption4());
+			old.setRightAnswer(question.getRightAnswer());
+			old.setDifficulty_level(question.getDifficulty_level());
+			old.setCategory(question.getCategory());
+			questionDao.save(old);
+
+		} else {
+			return question;
+		}
+
+		return old;
 	}
 
 }
