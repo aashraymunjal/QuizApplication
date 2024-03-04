6fc44abe4848d90cbdc09df3669d7ece8e23409a
Aashray Munjal <aashraymunjal@gmail.com>
2024-03-03 20:30:17 -0500

    added Response Entity and exception handling for Question Controller APIs

--- src/main/java/com/munjal/QuizApplication/Controller/QuestionController.java
+++ src/main/java/com/munjal/QuizApplication/Controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.munjal.QuizApplication.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,27 +24,27 @@ public class QuestionController {
 	private QuestionService questionService;
 
 	@GetMapping("allQuestions")
-	public List<Question> getAllQuestions() {
+	public ResponseEntity<List<Question>> getAllQuestions() {
 		return questionService.getAllQuestions();
 	}
 
 	@GetMapping("/category/{category}")
-	public List<Question> getQuestionsByCategory(@PathVariable("category") String category) {
+	public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable("category") String category) {
 		return questionService.getQuestionsByCategory(category);
 	}
 
 	@PostMapping("/addQuestion")
-	public String addQuestion(@RequestBody Question question) {
+	public ResponseEntity<String> addQuestion(@RequestBody Question question) {
 		return questionService.addQuestion(question);
 	}
 
 	@DeleteMapping("/delete/{id}")
-	public String delete(@PathVariable("id") int id) {
+	public ResponseEntity<String> delete(@PathVariable("id") int id) {
 		return questionService.delete(id);
 	}
 
 	@PutMapping("/update/{id}")
-	public Question updateQuestion(@PathVariable("id") int id, @RequestBody Question question) {
+	public ResponseEntity<Question> updateQuestion(@PathVariable("id") int id, @RequestBody Question question) {
 		return questionService.updateQuestion(id, question);
 
 	}
