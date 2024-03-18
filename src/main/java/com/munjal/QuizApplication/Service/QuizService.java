589fe7ecdb2a654e4e706f50ab0265d859f375c4
Aashray Munjal <aashraymunjal@gmail.com>
2024-03-18 14:43:44 -0400

    some changes

--- src/main/java/com/munjal/QuizApplication/Service/QuizService.java
+++ src/main/java/com/munjal/QuizApplication/Service/QuizService.java
@@ -3,14 +3,10 @@ package com.munjal.QuizApplication.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import com.munjal.QuizApplication.Controller.QuestionController;
 import com.munjal.QuizApplication.Dao.QuestionDao;
 import com.munjal.QuizApplication.Dao.QuizDao;
 import com.munjal.QuizApplication.Model.Question;
