980f2155a251ade631f868ae250f431399fc9bf3
Aashray Munjal <aashraymunjal@gmail.com>
2024-08-09 11:57:47 -0400

    Update QuestionService.java

--- src/main/java/com/munjal/QuizApplication/Service/QuestionService.java
+++ src/main/java/com/munjal/QuizApplication/Service/QuestionService.java
@@ -13,87 +13,51 @@ import com.munjal.QuizApplication.Model.Question;
 public class QuestionService {
 
 	@Autowired
-	private QuestionDao questionDao;
+	QuestionDao dao;
 
-	public ResponseEntity<List<Question>> getAllQuestions() {
-		try {
-			List<Question> questions = questionDao.findAll();
-			if (questions.size() > 0) {
-				return new ResponseEntity<>(questions, HttpStatus.OK);
-			} else {
-				return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
-			}
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+	public List<Question> getAllQuestions() {
+		return dao.findAll();
+	}
+
+	public Question adQuestion(Question question) {
+		return dao.save(question);
+	}
+
+	public List<Question> getQuestionByCat(String category) {
+
+		return dao.findByCategory(category);
+	}
+
+	public String deletebyid(int id) {
+
+		if (dao.findById(id).isPresent()) {
+			dao.deleteById(id);
+			return "Deleted";
+		} else {
+			return "Failed to find id";
 		}
 
 	}
 
-	public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
-		try {
-			List<Question> questions = questionDao.findBycategory(category);
-			if (questions != null) {
-				return new ResponseEntity<>(questions, HttpStatus.OK);
-			} else {
-				return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
-			}
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+	public Question upadteQuestion(int id, Question question) {
+
+		if (dao.findById(id).isPresent()) {
+			Optional<Question> temp = dao.findById(id);
+			Question saved = temp.get();
+			saved.setCategory(question.getCategory());
+			saved.setDifficulty_level(question.getDifficulty_level());
+			saved.setOption1(question.getOption1());
+			saved.setOption2(question.getOption2());
+			saved.setOption3(question.getOption3());
+			saved.setOption4(question.getOption4());
+
+			dao.save(saved);
+			return saved;
+		} else {
+			return null;
 		}
 
-	}
-
-	public ResponseEntity<String> addQuestion(Question question) {
-		try {
-			if (question != null) {
-				questionDao.save(question);
-				return new ResponseEntity<>("Added! " + question, HttpStatus.CREATED);
-			} else {
-				return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Empty question passed by user!");
-			}
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Something went wrong here!");
-		}
-
-	}
-
-	public ResponseEntity<String> delete(int id) {
-		try {
-			if (id > 0 && questionDao.findById(id).isPresent()) {
-				questionDao.deleteById(id);
-				return new ResponseEntity<>("Deleted id: " +id, HttpStatus.OK);
-			} else {
-				return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Question not present in DB!");
-			}
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Something went wrong here!");
-		}
-
-	}
-
-	public ResponseEntity<Question> updateQuestion(int id, Question question) {
-		try {
-			Optional<Question> q = questionDao.findById(id);
-			Question old = new Question();
-			if (questionDao.findById(id).isPresent()) {
-				old = q.get();
-				old.setQuestionTitle(question.getQuestionTitle());
-				old.setOption1(question.getOption1());
-				old.setOption2(question.getOption2());
-				old.setOption3(question.getOption3());
-				old.setOption4(question.getOption4());
-				old.setRightAnswer(question.getRightAnswer());
-				old.setDifficulty_level(question.getDifficulty_level());
-				old.setCategory(question.getCategory());
-				questionDao.save(old);
-				return new ResponseEntity<>(old, HttpStatus.OK);
-
-			} else {
-				return ResponseEntity.status(HttpStatus.NO_CONTENT).body(question);
-			}
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-		}
 	}
 
 }
+
